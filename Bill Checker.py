4823ac9958e1b0af5d1e88c83d355a0a7895cb66
princepro046 <princepro046@gmail.com>
2024-12-03 20:21:47 +0500

    first commit

---
+++ Bill Checker.py
@@ -0,0 +1,28 @@
+num1 = input("Enter Your Ride :")
+print = ("1.Bike")
+print = ("2.Car")
+
+user_choice = int(input("Enter Your Choice"))
+
+if( user_choice == 1):
+    print("Which Type Of Bike Do you prefer")
+    print("1.Kawasaki")
+    print("2.Ninja H2r")
+
+    user_choice2 = int(input("Enter Your Choice :"))
+    if (user_choice2 == 2 ):
+        print("You Have Selected Kawasaki")
+    else:
+        print("You Have Selected Ninja H2r :")
+if(user_choice == 2):
+     print("Which Type Of Car Do you prefer")
+     print("1.Rolls Royce Phantom") 
+     print("2.Pagani Huayrya x4560")
+
+user_choice2 = int(input("Enter Your Choice"))
+if (user_choice ==1):
+    print("you have selected Rolls Royce Phantom")
+else:
+    print("You have selected Pagani Huayrya x4560")
+        
+    
---
+++ Examinatioin Checker.py
@@ -0,0 +1,7 @@
+print("Enter only 'Y' for yes and 'N' for no")
+num1 = input("Do you have a medical certificate")
+if num1 == 'Y':
+    print("You are allowed")
+else:
+       input("Is your attendance over 75 :")
+       print("You are allowed")
